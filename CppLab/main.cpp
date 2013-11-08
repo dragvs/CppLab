6b6bc6e6b20fc76edf586ad0ba92ccfb4df4f658
dragvs <dragvs@gmail.com>
2013-11-08 13:58:37 +0400

    Initial Commit

---
+++ CppLab/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CppLab
+//
+//  Created by Vladimir Shishov on 11/8/13.
+//  Copyright (c) 2013 Vladimir Shishov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
